b48ceceaa2d73abfb87be3ef08a43a452de70c39
petro <petro25@gmail.com>
2016-11-04 22:50:49 +0200

    Some refactoring

--- url_checker.py
+++ url_checker.py
@@ -4,35 +4,56 @@ from bs4 import BeautifulSoup
 import requests
 import re
 from datetime import datetime
+import collections
 
 startTime = datetime.now()
 
-URL = 'http://volia.com'
+website = 'http://google.com'
+deep = 2
+
+
 list_links = []
 
-def create_list():
-    global new_list
-    new_list = []
-    #new_list is global list - possible to use in any place of this script
+class checker():
+    def urls(URL, DEEP):
+        site = requests.get(URL).text
+        soup = BeautifulSoup(site, "lxml")
+        links = soup.find_all('a')
+        for a in soup.find_all('a', href=True):
+            if re.search('^(http|https):\/\/', a['href']):
+                list_links.append(a['href'])
+            else:
+                list_links.append(URL + a['href'])
+        for x in list_links:
+            print("For link: ", x, "status code is: ", requests.get(x).status_code)
 
-def urls(URL):
-    site = requests.get(URL).text
-    soup = BeautifulSoup(site, "lxml")
-    links = soup.find_all('a')
-#    global list_links
-#    list_links = []
-    for a in soup.find_all('a', href=True):
-        if re.search('^(http|https):\/\/', a['href']):
-            list_links.append(a['href'])
-        else:
-            list_links.append(URL + a['href'])
+#checker.urls(website, deep)
 
-urls(URL)
 
-for x in list_links:
-    print("For link: ", x, "status code is: ", requests.get(x).status_code)
+count = 3
+urls = {}
+urls = collections.defaultdict(list)
 
-#r = requests.get('http://google.com')
-#print(r.status_code)
+def test(count):
+    с = 1
+    while с <= count:
+        site = requests.get('http://google.com').text
+        soup = BeautifulSoup(site, "lxml")
+        links = soup.find_all('a')
+        for a in soup.find_all('a', href=True):
+            if re.search('^(http|https):\/\/', a['href']):
+                urls[с].append(a['href'])
+            else:
+                urls[с].append('http://google.com' + a['href'])
+        с += 1
+
+test(count)
+print(urls[1])
+
+
+
+
+#link = 'link' + str(1)
+#print(link)
 
 print(datetime.now() - startTime)
